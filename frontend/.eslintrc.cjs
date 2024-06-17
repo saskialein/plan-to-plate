9438b958c36836d26b20d8aa8437e6606f10ad4d
Saskia Leinberger <66111428+saskialein@users.noreply.github.com>
2024-06-17 19:14:40 +1200

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,14 +5,38 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'error',
+    'react/destructuring-assignment': [
+      'error',
+      'always',
+      {
+        destructureInSignature: 'always',
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
