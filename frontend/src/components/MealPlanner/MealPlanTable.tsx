dce06c53a9555b30ce326af9c4e5459870d079ff
Saskia Leinberger <66111428+saskialein@users.noreply.github.com>
2024-07-02 14:27:24 +1200

    Add a Save button in MealPlanTable.tsx

--- frontend/src/components/MealPlanner/MealPlanTable.tsx
+++ frontend/src/components/MealPlanner/MealPlanTable.tsx
@@ -1,4 +1,15 @@
-import { Link, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Link,
+  Stack,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
 import { Link as RouterLink } from '@tanstack/react-router'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { formatTitleToUrl } from '../../utils/route'
@@ -18,79 +29,91 @@ const openInNewTab = (recipe: Meal) => {
 
 export function MealPlanTable({ plan }: MealPlanTableProps) {
   return (
-    <Table variant="simple">
-      <Thead>
-        <Tr>
-          <Th>Day</Th>
-          <Th>Breakfast</Th>
-          <Th>Lunch</Th>
-          <Th>Dinner</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {Object.keys(plan).map((day) => (
-          <Tr key={day}>
-            <Td>{day.charAt(0).toUpperCase() + day.slice(1)}</Td>
-            <Td>
-              {plan[day].breakfast.url ? (
-                <Link
-                  href={plan[day].breakfast.url ?? ''}
-                  color="teal.500"
-                  isExternal
-                >
-                  {plan[day].breakfast.recipe} <ExternalLinkIcon mx="2px" />
-                </Link>
-              ) : (
-                <Link
-                  as={RouterLink}
-                  color="teal.500"
-                  onClick={() => openInNewTab(plan[day].breakfast)}
-                >
-                  {plan[day].breakfast.recipe}
-                </Link>
-              )}
-            </Td>
-            <Td>
-              {plan[day].lunch.url ? (
-                <Link
-                  href={plan[day].lunch.url ?? ''}
-                  color="teal.500"
-                  isExternal
-                >
-                  {plan[day].lunch.recipe} <ExternalLinkIcon mx="2px" />
-                </Link>
-              ) : (
-                <Link
-                  as={RouterLink}
-                  onClick={() => openInNewTab(plan[day].lunch)}
-                  color="teal.500"
-                >
-                  {plan[day].lunch.recipe}
-                </Link>
-              )}
-            </Td>
-            <Td>
-              {plan[day].dinner.url ? (
-                <Link
-                  href={plan[day].dinner.url ?? ''}
-                  color="teal.500"
-                  isExternal
-                >
-                  {plan[day].dinner.recipe} <ExternalLinkIcon mx="2px" />
-                </Link>
-              ) : (
-                <Link
-                  as={RouterLink}
-                  color="teal.500"
-                  onClick={() => openInNewTab(plan[day].dinner)}
-                >
-                  {plan[day].dinner.recipe}
-                </Link>
-              )}
-            </Td>
+    <Stack spacing={8} alignItems="center" mb={8}>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Day</Th>
+            <Th>Breakfast</Th>
+            <Th>Lunch</Th>
+            <Th>Dinner</Th>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
+        </Thead>
+        <Tbody>
+          {Object.keys(plan).map((day) => (
+            <Tr key={day}>
+              <Td>{day.charAt(0).toUpperCase() + day.slice(1)}</Td>
+              <Td>
+                {plan[day].breakfast.url ? (
+                  <Link
+                    href={plan[day].breakfast.url ?? ''}
+                    color="teal.500"
+                    isExternal
+                  >
+                    {plan[day].breakfast.recipe} <ExternalLinkIcon mx="2px" />
+                  </Link>
+                ) : (
+                  <Link
+                    as={RouterLink}
+                    color="teal.500"
+                    onClick={() => openInNewTab(plan[day].breakfast)}
+                  >
+                    {plan[day].breakfast.recipe}
+                  </Link>
+                )}
+              </Td>
+              <Td>
+                {plan[day].lunch.url ? (
+                  <Link
+                    href={plan[day].lunch.url ?? ''}
+                    color="teal.500"
+                    isExternal
+                  >
+                    {plan[day].lunch.recipe} <ExternalLinkIcon mx="2px" />
+                  </Link>
+                ) : (
+                  <Link
+                    as={RouterLink}
+                    onClick={() => openInNewTab(plan[day].lunch)}
+                    color="teal.500"
+                  >
+                    {plan[day].lunch.recipe}
+                  </Link>
+                )}
+              </Td>
+              <Td>
+                {plan[day].dinner.url ? (
+                  <Link
+                    href={plan[day].dinner.url ?? ''}
+                    color="teal.500"
+                    isExternal
+                  >
+                    {plan[day].dinner.recipe} <ExternalLinkIcon mx="2px" />
+                  </Link>
+                ) : (
+                  <Link
+                    as={RouterLink}
+                    color="teal.500"
+                    onClick={() => openInNewTab(plan[day].dinner)}
+                  >
+                    {plan[day].dinner.recipe}
+                  </Link>
+                )}
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <Box>
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          size="md"
+          onClick={() => console.log(plan)}
+        >
+          Save Meal Plan
+        </Button>
+      </Box>
+    </Stack>
   )
 }
