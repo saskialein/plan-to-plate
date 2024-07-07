99177439c61100ba2c6d534787f1b196f83ae1d1
Saskia Leinberger <66111428+saskialein@users.noreply.github.com>
2024-07-07 17:33:55 +1200

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
 import { Container, Stack, Text } from '@chakra-ui/react'
+import useAuth from '../hooks/useAuth'
 
 export function Dashboard() {
+  const { user } = useAuth()
+
   return (
     <>
       <Container maxW="full">
         <Stack pt={12} spacing={4}>
-          <Text fontSize="2xl">Hi 👋</Text>
+          <Text fontSize="2xl">Hi {user?.fullName} 👋</Text>
         </Stack>
       </Container>
     </>
